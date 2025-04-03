619492a34732e2ff28ddbf2c1904dff1e2753d99
purplesm0ke <PolinaSt.2003@yandex.ru>
2025-04-03 21:21:29 +0300

    started to do calculating processes

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ const buttonList = document.querySelector('ul');
 const buttons = buttonList.childNodes
 const inputField = document.querySelector('.calculation')
 
-
+const operationIds = ['divide', 'multiply', 'subtract', 'add']
 
 buttonList.addEventListener('click', (event) => {
     console.log(event.target.textContent)
@@ -15,4 +15,17 @@ buttonList.addEventListener('click', (event) => {
     if(event.target.id === "clear"){
         inputField.value = ''
     }
+    if (event.target.id === "equals"){
+        console.log("result", inputField.value)
+        for (let i = 0; i < inputField.value.length; i++) {
+            if(isValidNumber(inputField.value[i]) && event.target.id === "equals"){
+                let calculation = inputField.value + inputField.value
+                console.log('cal', calculation)
+            }
+        }
+    }
+    if(operationIds.includes(event.target.id)){
+        inputField.value += value
+    }
+
 })
